bda999afd991ee3b3e0334c8cf29b2170621cc19
Chiayi <chia90080@gmail.com>
2018-01-08 16:43:44 +0800

    888

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define a 555555555555
+#define a 88888
 int main()
 {
     printf("878787.\n");printf("878787.\n");printf("878787.\n");printf("878787.\n");printf("878787.\n");
